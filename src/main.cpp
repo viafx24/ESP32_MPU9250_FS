eafa02c5d6783db5cd015ab31f02667792dc1642
Guillaume Baptist <guillaume.baptist@free.fr>
2021-08-29 10:37:40 +0200

    nettoyage de l'inutile

--- src/main.cpp
+++ src/main.cpp
@@ -16,39 +16,6 @@ WiFiServer server(80);
 
 MPU9250 mpu;
 
-void print_calibration() {
-    Serial.println("< calibration parameters >");
-    Serial.println("accel bias [g]: ");
-    Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
-    Serial.print(", ");
-    Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
-    Serial.print(", ");
-    Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
-    Serial.println();
-    Serial.println("gyro bias [deg/s]: ");
-    Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
-    Serial.print(", ");
-    Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
-    Serial.print(", ");
-    Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
-    Serial.println();
-    Serial.println("mag bias [mG]: ");
-    Serial.print(mpu.getMagBiasX());
-    Serial.print(", ");
-    Serial.print(mpu.getMagBiasY());
-    Serial.print(", ");
-    Serial.print(mpu.getMagBiasZ());
-    Serial.println();
-    Serial.println("mag scale []: ");
-    Serial.print(mpu.getMagScaleX());
-    Serial.print(", ");
-    Serial.print(mpu.getMagScaleY());
-    Serial.print(", ");
-    Serial.print(mpu.getMagScaleZ());
-    Serial.println();
-}
-
-
 void setup()
 {
 
@@ -80,21 +47,6 @@ void setup()
 
   server.begin();
 
-    // // calibrate anytime you want to
-    // Serial.println("Accel Gyro calibration will start in 5sec.");
-    // Serial.println("Please leave the device still on the flat plane.");
-    // mpu.verbose(true);
-    // delay(5000);
-    // mpu.calibrateAccelGyro();
-
-    // Serial.println("Mag calibration will start in 5sec.");
-    // Serial.println("Please Wave device in a figure eight until done.");
-    // delay(5000);
-    // mpu.calibrateMag();
-
-    // print_calibration();
-    // mpu.verbose(false);
-
 
 mpu.setAccBias(1013.52, 19.45, 996.83);
 mpu.setGyroBias(0.09, 1.55, 4.78);
@@ -123,11 +75,6 @@ void loop()
       if (mpu.update())
       {
 
-        // currentLine = String(millis()) + "," + String(mpu.getAccX()) + "," + String(mpu.getAccY()) + "," + String(mpu.getAccZ()) +
-        //               "," + String(mpu.getGyroX()) + "," + String(mpu.getGyroY()) + "," + String(mpu.getGyroZ()) +
-        //               "," + String(mpu.getMagX()) + "," + String(mpu.getMagY()) + "," + String(mpu.getMagZ());
-
-        
         currentLine = String(millis()) + "," + String(mpu.getAccX()) + "," + String(mpu.getAccY()) + "," + String(mpu.getAccZ()) +
                       "," + String(mpu.getGyroX()) + "," + String(mpu.getGyroY()) + "," + String(mpu.getGyroZ()) +
                       "," + String(mpu.getMagX()) + "," + String(mpu.getMagY()) + "," + String(mpu.getMagZ()) +
@@ -135,11 +82,7 @@ void loop()
                       "," + String(mpu.getQuaternionX()) + "," + String(mpu.getQuaternionY()) + "," + String(mpu.getQuaternionZ()) +
                        "," + String(mpu.getQuaternionW()) ;
 
-        
-
-
-
-
+ 
         client.println(currentLine);
         delay(10);
       }
@@ -152,38 +95,3 @@ void loop()
 
   }
 }
-  // #include "MPU9250.h"
-
-  // MPU9250 mpu;
-
-  // void setup() {
-  //     Serial.begin(115200);
-  //     Wire.begin();
-  //     delay(2000);
-
-  //     if (!mpu.setup(0x68)) {  // change to your own address
-  //         while (1) {
-  //             Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
-  //             delay(5000);
-  //         }
-  //     }
-  // }
-
-  // void loop() {
-  //     if (mpu.update()) {
-  //         static uint32_t prev_ms = millis();
-  //         if (millis() > prev_ms + 25) {
-  //             print_roll_pitch_yaw();
-  //             prev_ms = millis();
-  //         }
-  //     }
-  // }
-
-  // void print_roll_pitch_yaw() {
-  //     Serial.print("Yaw, Pitch, Roll: ");
-  //     Serial.print(mpu.getYaw(), 2);
-  //     Serial.print(", ");
-  //     Serial.print(mpu.getPitch(), 2);
-  //     Serial.print(", ");
-  //     Serial.println(mpu.getRoll(), 2);
-  // }
